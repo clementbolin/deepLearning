b4ca376426e2a430d3050aa0b6ca52f3a65659fd
Bolin <clement.bolin@epitech.eu>
2020-09-20 20:02:02 +0200

    build classifier and display mean and variance

--- main.py
+++ main.py
@@ -108,3 +108,21 @@ if new_prediction == False:
 else:
     new_prediction = "he leave bank"
 print("prediction result : " + str(new_prediction))
+
+def build_classifier(optimizer='adam'):
+    classifier = Sequential()
+    classifier.add(Dense(units = 6, kernel_initializer = 'uniform', activation = 'relu', input_dim = 11))
+    classifier.add(Dense(units = 6, kernel_initializer = 'uniform', activation = 'relu'))
+    classifier.add(Dense(units = 1, kernel_initializer = 'uniform', activation = 'sigmoid'))
+    classifier.compile(optimizer = optimizer, loss = 'binary_crossentropy', metrics = ['accuracy'])
+    return classifier
+
+# Evaluate
+from keras.wrappers.scikit_learn import KerasClassifier
+from sklearn.model_selection import cross_val_score
+classifier = KerasClassifier(build_fn = build_classifier, batch_size = 10, epochs = 100)
+accuracies = cross_val_score(estimator = classifier, X = X_train, y = y_train, 
+                             cv = 10, n_jobs = -1)
+mean = accuracies.mean()
+variance = accuracies.std()
+print("mean = " + str(mean) + " variance = " + str(variance))
